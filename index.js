b7d1ea5858a35dd7e209f87971cf55e45785083c
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-13 09:19:52 +0000

    finish excercise

---
+++ index.js
@@ -0,0 +1,20 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a -b ;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
+
+const person = {
+    firstName: "Somboon",
+    lastName: "Nontaganok",
+    age: 30,
+    location: "Bangkok",
+    hobby: "running" 
+}
+
+console.log("Hi, my name is " + person.firstName + " " + person.lastName + ". I live in " + person.location + " and enjoy " + person.hobby + ".");
+
